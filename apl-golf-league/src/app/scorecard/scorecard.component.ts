1e8e2f8eb3c1c02b0b9379abce1dca7e7aca8a29
Andris Jaunzemis <adjaunzemis@gmail.com>
2021-02-15 08:48:37 -0500

    Updated scorecard component to apply correct styling based on round results.

--- apl-golf-league/src/app/scorecard/scorecard.component.ts
+++ apl-golf-league/src/app/scorecard/scorecard.component.ts
@@ -7,16 +7,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScorecardComponent implements OnInit {
 
-  holes = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  pars = [5, 4, 5, 4, 3, 4, 4, 3, 4];
-  yardages = [510, 380, 510, 380, 155, 380, 380, 155, 380];
-  handicaps = [9, 8, 7, 6, 5, 4, 3, 2, 1];
-  grossScores = [2, 2, 4, 4, 4, 6, 7, 3, 4];
-  netScores = [2, 2, 4, 4, 4, 6, 7, 4, 3];
+  holes = [
+    new GolfHole(1, 5, 9, 510, 2, 2),
+    new GolfHole(2, 4, 8, 380, 2, 2),
+    new GolfHole(3, 5, 7, 510, 4, 4),
+    new GolfHole(4, 4, 6, 380, 4, 4),
+    new GolfHole(5, 3, 5, 155, 4, 4),
+    new GolfHole(6, 4, 4, 380, 6, 6),
+    new GolfHole(7, 4, 3, 380, 7, 7),
+    new GolfHole(8, 3, 2, 155, 3, 4),
+    new GolfHole(9, 4, 1, 380, 4, 3)
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getScoreClass(score: number, par: number): string {
+    const scoreToPar = score - par;
+    if (scoreToPar < -2) {
+      return "'circle crosshatch'";
+    } else if (scoreToPar < -1) {
+      return "'circle'";
+    } else if (scoreToPar == 0) {
+      return "";
+    } else if (scoreToPar == 1) {
+      return "'square'";
+    } else {
+      return "'square crosshatch'";
+    }
+  }
+
+  computeTotalPar(): number {
+    return this.holes.reduce(function(prev, cur) {
+      return prev + cur.par;
+    }, 0);
+  }
+
+  computeTotalYardage(): number {
+    return this.holes.reduce(function(prev, cur) {
+      return prev + cur.yardage;
+    }, 0);
+  }
+
+  computeTotalNetScore(): number {
+    return this.holes.reduce(function(prev, cur) {
+      return prev + cur.netScore;
+    }, 0);
+  }
+
+  computeTotalGrossScore(): number {
+    return this.holes.reduce(function(prev, cur) {
+      return prev + cur.grossScore;
+    }, 0);
+  }
+
+}
+
+class GolfHole {
+  number: number;
+  par: number;
+  handicap: number;
+  yardage: number;
+  grossScore: number;
+  netScore: number;
+  handicapStrokes: number;
+
+  constructor(number: number, par: number, handicap: number, yardage: number, grossScore: number, netScore: number) {
+    this.number = number;
+    this.par = par;
+    this.handicap = handicap;
+    this.yardage = yardage;
+    this.grossScore = grossScore;
+    this.netScore = netScore;
+
+    this.handicapStrokes = grossScore - netScore;
+  }
 }
