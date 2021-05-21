abd5527ff5d46d2b9d096ac11108309a36cb3f2a
Andris Jaunzemis <adjaunzemis@gmail.com>
2021-05-20 21:09:53 -0400

    Added courses service with mock course list. Implemented connection to service in courses list.

--- angular-material/src/app/courses/course-list/course-list.component.ts
+++ angular-material/src/app/courses/course-list/course-list.component.ts
@@ -1,10 +1,36 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from "rxjs";
+
+import { CoursesService } from "../courses.service";
+import { GolfCourse } from "../../shared/golf-course.model";
 
 @Component({
   selector: "app-course-list",
   templateUrl: "./course-list.component.html",
   styleUrls: ["./course-list.component.css"]
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit, OnDestroy {
+  courses: GolfCourse[] = [];
+  private coursesSub: Subscription;
+
+  totalCourses = 0;
+  coursesPerPage = 5;
+  currentPage = 1;
+  pageSizeOptions = [5, 10, 20];
+
+  constructor(private coursesService: CoursesService) {}
+
+  ngOnInit(): void {
+    this.coursesSub = this.coursesService.getCourseUpdateListener()
+      .subscribe((courseData: { courses: GolfCourse[], courseCount: number }) => {
+        this.courses = courseData.courses;
+        this.totalCourses = courseData.courseCount;
+      });
+    this.coursesService.getCourses();
+  }
+
+  ngOnDestroy(): void {
+    this.coursesSub.unsubscribe();
+  }
 
 }
---
+++ angular-material/src/app/courses/courses.service.ts
@@ -0,0 +1,131 @@
+import { Injectable } from "@angular/core";
+import { Observable, Subject } from "rxjs";
+
+import { GolfCourse } from "../shared/golf-course.model"
+
+const MOCK_COURSES : GolfCourse[] = [
+  {
+    id: 123,
+    name: "Mock Course",
+    abbreviation: "MC",
+    city: "Mock City",
+    state: "MS",
+    zipCode: 12345,
+    phone: "123-456-7890",
+    website: "www.google.com",
+    tracks: [
+      {
+        id: 111,
+        courseId: 123,
+        name: "Front",
+        abbreviation: "F",
+        teeSets: [
+          {
+            id: 0,
+            trackId: 111,
+            name: "White",
+            color: "#FFFFFF",
+            gender: "M",
+            rating: 70.1,
+            slope: 120,
+            holes: [
+              {
+                id: 1,
+                teeSetId: 0,
+                number: 1,
+                par: 4,
+                handicap: 9,
+                yardage: 395
+              },
+              {
+                id: 2,
+                teeSetId: 0,
+                number: 2,
+                par: 4,
+                handicap: 9,
+                yardage: 395
+              },
+              {
+                id: 3,
+                teeSetId: 0,
+                number: 3,
+                par: 4,
+                handicap: 9,
+                yardage: 395
+              },
+              {
+                id: 4,
+                teeSetId: 0,
+                number: 4,
+                par: 4,
+                handicap: 9,
+                yardage: 395
+              },
+              {
+                id: 5,
+                teeSetId: 0,
+                number: 5,
+                par: 4,
+                handicap: 9,
+                yardage: 395
+              },
+              {
+                id: 6,
+                teeSetId: 0,
+                number: 6,
+                par: 4,
+                handicap: 9,
+                yardage: 395
+              },
+              {
+                id: 7,
+                teeSetId: 0,
+                number: 7,
+                par: 4,
+                handicap: 9,
+                yardage: 395
+              },
+              {
+                id: 8,
+                teeSetId: 0,
+                number: 8,
+                par: 4,
+                handicap: 9,
+                yardage: 395
+              },
+              {
+                id: 9,
+                teeSetId: 0,
+                number: 9,
+                par: 4,
+                handicap: 9,
+                yardage: 395
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+]
+
+@Injectable({
+  providedIn: "root"
+})
+export class CoursesService {
+  private courses: GolfCourse[] = [];
+  private coursesUpdated = new Subject<{ courses: GolfCourse[], courseCount: number }>();
+
+  getCourses(): void {
+    // TODO: use http.get<GolfCourse[]>()
+    this.coursesUpdated.next({
+      courses: [...MOCK_COURSES],
+      courseCount: MOCK_COURSES.length
+    });
+  }
+
+  getCourseUpdateListener() : Observable<{ courses: GolfCourse[], courseCount: number }> {
+    return this.coursesUpdated.asObservable();
+  }
+
+}
