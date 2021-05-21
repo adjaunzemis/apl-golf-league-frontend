8c2ac8736062b1c5faed99f3e83600b5fdeecd70
Andris Jaunzemis <adjaunzemis@gmail.com>
2021-05-20 22:45:05 -0400

    Created prototype course list display with expansion panels. Added supporting material imports.

--- angular-material/src/app/angular-material.module.ts
+++ angular-material/src/app/angular-material.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   exports: [
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatPaginatorModule
   ]
 })
 export class AngularMaterialModule {}
--- angular-material/src/app/courses/courses.service.ts
+++ angular-material/src/app/courses/courses.service.ts
@@ -3,112 +3,6 @@ import { Observable, Subject } from "rxjs";
 
 import { GolfCourse } from "../shared/golf-course.model"
 
-const MOCK_COURSES : GolfCourse[] = [
-  {
-    id: 123,
-    name: "Mock Course",
-    abbreviation: "MC",
-    city: "Mock City",
-    state: "MS",
-    zipCode: 12345,
-    phone: "123-456-7890",
-    website: "www.google.com",
-    tracks: [
-      {
-        id: 111,
-        courseId: 123,
-        name: "Front",
-        abbreviation: "F",
-        teeSets: [
-          {
-            id: 0,
-            trackId: 111,
-            name: "White",
-            color: "#FFFFFF",
-            gender: "M",
-            rating: 70.1,
-            slope: 120,
-            holes: [
-              {
-                id: 1,
-                teeSetId: 0,
-                number: 1,
-                par: 4,
-                handicap: 9,
-                yardage: 395
-              },
-              {
-                id: 2,
-                teeSetId: 0,
-                number: 2,
-                par: 4,
-                handicap: 9,
-                yardage: 395
-              },
-              {
-                id: 3,
-                teeSetId: 0,
-                number: 3,
-                par: 4,
-                handicap: 9,
-                yardage: 395
-              },
-              {
-                id: 4,
-                teeSetId: 0,
-                number: 4,
-                par: 4,
-                handicap: 9,
-                yardage: 395
-              },
-              {
-                id: 5,
-                teeSetId: 0,
-                number: 5,
-                par: 4,
-                handicap: 9,
-                yardage: 395
-              },
-              {
-                id: 6,
-                teeSetId: 0,
-                number: 6,
-                par: 4,
-                handicap: 9,
-                yardage: 395
-              },
-              {
-                id: 7,
-                teeSetId: 0,
-                number: 7,
-                par: 4,
-                handicap: 9,
-                yardage: 395
-              },
-              {
-                id: 8,
-                teeSetId: 0,
-                number: 8,
-                par: 4,
-                handicap: 9,
-                yardage: 395
-              },
-              {
-                id: 9,
-                teeSetId: 0,
-                number: 9,
-                par: 4,
-                handicap: 9,
-                yardage: 395
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-]
-
 @Injectable({
   providedIn: "root"
 })
@@ -119,8 +13,8 @@ export class CoursesService {
   getCourses(): void {
     // TODO: use http.get<GolfCourse[]>()
     this.coursesUpdated.next({
-      courses: [...MOCK_COURSES],
-      courseCount: MOCK_COURSES.length
+      courses: [MOCK_COURSE, MOCK_COURSE],
+      courseCount: 2
     });
   }
 
@@ -128,4 +22,191 @@ export class CoursesService {
     return this.coursesUpdated.asObservable();
   }
 
+}
+
+const MOCK_COURSE : GolfCourse = {
+  id: 123,
+  name: "Mock Course",
+  abbreviation: "MC",
+  city: "Mock City",
+  state: "MS",
+  zipCode: 12345,
+  phone: "123-456-7890",
+  website: "www.google.com",
+  tracks: [
+    {
+      id: 111,
+      courseId: 123,
+      name: "Front",
+      abbreviation: "F",
+      teeSets: [
+        {
+          id: 0,
+          trackId: 111,
+          name: "White",
+          color: "#00ff00",
+          gender: "M",
+          rating: 70.1,
+          slope: 120,
+          holes: [
+            {
+              id: 1,
+              teeSetId: 0,
+              number: 1,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            },
+            {
+              id: 2,
+              teeSetId: 0,
+              number: 2,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            },
+            {
+              id: 3,
+              teeSetId: 0,
+              number: 3,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            },
+            {
+              id: 4,
+              teeSetId: 0,
+              number: 4,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            },
+            {
+              id: 5,
+              teeSetId: 0,
+              number: 5,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            },
+            {
+              id: 6,
+              teeSetId: 0,
+              number: 6,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            },
+            {
+              id: 7,
+              teeSetId: 0,
+              number: 7,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            },
+            {
+              id: 8,
+              teeSetId: 0,
+              number: 8,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            },
+            {
+              id: 9,
+              teeSetId: 0,
+              number: 9,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            }
+          ]
+        },
+        {
+          id: 0,
+          trackId: 111,
+          name: "Blue",
+          color: "#0000ff",
+          gender: "M",
+          rating: 72.3,
+          slope: 124,
+          holes: [
+            {
+              id: 1,
+              teeSetId: 0,
+              number: 1,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            },
+            {
+              id: 2,
+              teeSetId: 0,
+              number: 2,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            },
+            {
+              id: 3,
+              teeSetId: 0,
+              number: 3,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            },
+            {
+              id: 4,
+              teeSetId: 0,
+              number: 4,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            },
+            {
+              id: 5,
+              teeSetId: 0,
+              number: 5,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            },
+            {
+              id: 6,
+              teeSetId: 0,
+              number: 6,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            },
+            {
+              id: 7,
+              teeSetId: 0,
+              number: 7,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            },
+            {
+              id: 8,
+              teeSetId: 0,
+              number: 8,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            },
+            {
+              id: 9,
+              teeSetId: 0,
+              number: 9,
+              par: 4,
+              handicap: 9,
+              yardage: 395
+            }
+          ]
+        }
+      ]
+    }
+  ]
 }
