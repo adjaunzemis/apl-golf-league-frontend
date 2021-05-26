9e647ca30774c51d0ca6a8cc79792731fd9754f2
Andris Jaunzemis <adjaunzemis@gmail.com>
2021-05-26 08:02:33 -0400

    Added http client to courses service, connected to (local) backend server.

--- apl-golf-league/src/app/app.module.ts
+++ apl-golf-league/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AngularMaterialModule } from './angular-material.module';
 import { HeaderComponent } from './header/header.component';
 import { CoursesModule } from './courses/courses.module';
-
 
 @NgModule({
   declarations: [
@@ -17,6 +17,7 @@ import { CoursesModule } from './courses/courses.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
     AngularMaterialModule,
     CoursesModule
