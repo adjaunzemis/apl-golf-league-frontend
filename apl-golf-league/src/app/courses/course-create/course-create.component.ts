e4f739d5c733b66ca03ad051cc78247c39e9c637
Andris Jaunzemis <adjaunzemis@gmail.com>
2021-06-08 23:11:08 -0400

    Added input fields for course create page. Bound to course data to allow display of existing course data.

--- apl-golf-league/src/app/courses/course-create/course-create.component.ts
+++ apl-golf-league/src/app/courses/course-create/course-create.component.ts
@@ -1,66 +1,87 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Subscription } from "rxjs";
 
 import { GolfCourse } from "src/app/shared/golf-course.model";
 import { GolfTrack } from 'src/app/shared/golf-track.model';
 import { GolfTeeSet } from '../../shared/golf-tee-set.model';
+import { CoursesService } from "../courses.service";
 
 @Component({
     selector: "app-course-create",
     templateUrl: "./course-create.component.html",
     styleUrls: ["./course-create.component.css"]
 })
-export class CourseCreateComponent {
-    courseData: GolfCourse = {
+export class CourseCreateComponent implements OnInit, OnDestroy {
+    course: GolfCourse = {
         id: -1,
         name: "",
         abbreviation: ""
     }
+    private coursesSub: Subscription;
 
-    onAddCourse(form: NgForm): void {
+    constructor(private coursesService: CoursesService) {}
+
+    ngOnInit(): void {
+        this.coursesSub = this.coursesService.getCourseUpdateListener()
+          .subscribe((courseData: { courses: GolfCourse[], courseCount: number }) => {
+            this.course = courseData.courses[0];
+          });
+        this.coursesService.getCourses();
+    }
+
+    ngOnDestroy(): void {
+        this.coursesSub.unsubscribe();
+    }
+
+    onSubmitCourse(form: NgForm): void {
+        console.log(form.value)
         if (form.valid) {
             console.log("Adding course: " + form.value.courseName);
-            this.onClearCourse(form); // TODO: validate entries and connect to database to add course data
+
+            // TODO: validate entries and connect to database to add course data
+
+            this.onClearCourse(form);
         }
     }
 
     onClearCourse(form: NgForm): void {
-        this.courseData.tracks = [];
+        this.course.tracks = [];
         form.reset();
     }
 
     onAddTrack(): void {
         const newTrack: GolfTrack = {
             id: -1,
-            courseId: -1,
+            courseId: this.course.id,
             name: "",
             abbreviation: ""
         };
-        if (!this.courseData.tracks) {
-            this.courseData.tracks = [];
+        if (!this.course.tracks) {
+            this.course.tracks = [];
         }
-        this.courseData.tracks.push(newTrack);
+        this.course.tracks.push(newTrack);
     }
 
     onRemoveTrack(trackToRemove: GolfTrack): void {
-        if (this.courseData.tracks) {
-            const trackIndex = this.courseData.tracks?.findIndex((track) => {
+        if (this.course.tracks) {
+            const trackIndex = this.course.tracks?.findIndex((track) => {
                 return track === trackToRemove;
             });
             if (trackIndex > -1) {
-                this.courseData.tracks.splice(trackIndex, 1);
+                this.course.tracks.splice(trackIndex, 1);
             }
         }
     }
 
     onAddTeeSet(track: GolfTrack): void {
-        let trackData = this.courseData.tracks?.find((t) => {
+        let trackData = this.course.tracks?.find((t) => {
             return t === track;
         });
         if (trackData) {
             const newTeeSet: GolfTeeSet = {
                 id: -1,
-                trackId: -1,
+                trackId: track.id,
                 name: "",
                 color: "",
                 gender: "",
@@ -75,7 +96,7 @@ export class CourseCreateComponent {
     }
 
     onRemoveTeeSet(teeSetToRemove: GolfTeeSet): void {
-        this.courseData.tracks?.forEach((track) => {
+        this.course.tracks?.forEach((track) => {
             if (track.teeSets) {
                 const teeSetIndex = track.teeSets.findIndex((teeSet) => {
                     return teeSet === teeSetToRemove;
