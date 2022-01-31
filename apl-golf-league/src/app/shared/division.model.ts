f920c5230b0f3b0fa2213d02daf1d41f3a6cc33e
Andris Jaunzemis <adjaunzemis@gmail.com>
2022-01-31 09:58:29 -0500

    Added track name to division data display

--- apl-golf-league/src/app/shared/division.model.ts
+++ apl-golf-league/src/app/shared/division.model.ts
@@ -3,9 +3,10 @@ export interface DivisionData {
     flight_id: number
     name: string
     gender: string
-    home_tee_name?: number
-    home_tee_rating?: number
-    home_tee_slope?: number
+    track_name: string
+    tee_name: string
+    tee_rating: number
+    tee_slope: number
 }
 
 export interface TournamentDivisionData {
@@ -13,10 +14,12 @@ export interface TournamentDivisionData {
     flight_id: number
     name: string
     gender: string
-    primary_tee_name: number
+    primary_track_name: string
+    primary_tee_name: string
     primary_tee_rating: number
     primary_tee_slope: number
-    secondary_tee_name: number
+    secondary_track_name: string
+    secondary_tee_name: string
     secondary_tee_rating: number
     secondary_tee_slope: number
 }
