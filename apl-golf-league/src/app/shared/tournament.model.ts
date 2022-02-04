7a2f8cbfb1716fe8ea15bafc03bc7675158bb8ee
Andris Jaunzemis <adjaunzemis@gmail.com>
2022-02-04 07:53:03 -0500

    Added tournament date to tournament data model
    Fixed bugs in tournament round data display for missing rounds.

--- apl-golf-league/src/app/shared/tournament.model.ts
+++ apl-golf-league/src/app/shared/tournament.model.ts
@@ -4,6 +4,7 @@ import { TournamentTeamData } from "./team.model";
 export interface TournamentData {
     tournament_id: number
     year: number
+    date: Date
     name: string
     logo_url?: string
     secretary: string
