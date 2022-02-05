7d271845e3242d0ce84dce963627695473dd9281
Andris Jaunzemis <adjaunzemis@gmail.com>
2022-02-05 15:58:56 -0500

    Enforced match scorecard focus on focused team

--- apl-golf-league/src/app/matches/match-scorecard/match-scorecard-score-line/match-scorecard-score-line.component.ts
+++ apl-golf-league/src/app/matches/match-scorecard/match-scorecard-score-line/match-scorecard-score-line.component.ts
@@ -11,14 +11,14 @@ import { RoundData } from "src/app/shared/round.model";
 export class MatchScorecardScoreLineComponent implements OnInit, OnChanges {
   @Input() match: MatchData;
 
-  @Input() homeTeamId: number;
-  @Input() awayTeamId: number;
+  @Input() topTeamId: number;
+  @Input() bottomTeamId: number;
 
-  homeTeamRounds: RoundData[];
-  awayTeamRounds: RoundData[];
+  topTeamRounds: RoundData[];
+  bottomTeamRounds: RoundData[];
 
-  homeTeamHoleNetScores: number[];
-  awayTeamHoleNetScores: number[];
+  topTeamHoleNetScores: number[];
+  bottomTeamHoleNetScores: number[];
 
   winningTeamId: number;
 
@@ -33,39 +33,39 @@ export class MatchScorecardScoreLineComponent implements OnInit, OnChanges {
   }
 
   private separateRoundsByTeam() {
-    this.homeTeamRounds = [];
-    this.awayTeamRounds = [];
+    this.topTeamRounds = [];
+    this.bottomTeamRounds = [];
     for (let round of this.match.rounds) {
-      if (round.team_id === this.homeTeamId) {
-        this.homeTeamRounds.push(round);
-      } else if (round.team_id === this.awayTeamId) {
-        this.awayTeamRounds.push(round);
+      if (round.team_id === this.topTeamId) {
+        this.topTeamRounds.push(round);
+      } else if (round.team_id === this.bottomTeamId) {
+        this.bottomTeamRounds.push(round);
       }
     }
   }
 
   private computeHoleNetScores() {
-    this.homeTeamHoleNetScores = [];
-    this.awayTeamHoleNetScores = [];
+    this.topTeamHoleNetScores = [];
+    this.bottomTeamHoleNetScores = [];
     for (let holeIdx = 0; holeIdx < this.match.rounds[0].holes.length; holeIdx++) {
-      this.homeTeamHoleNetScores[holeIdx] = 0;
-      for (let round of this.homeTeamRounds) {
-        this.homeTeamHoleNetScores[holeIdx] += round.holes[holeIdx].net_score;
+      this.topTeamHoleNetScores[holeIdx] = 0;
+      for (let round of this.topTeamRounds) {
+        this.topTeamHoleNetScores[holeIdx] += round.holes[holeIdx].net_score;
       }
-      this.awayTeamHoleNetScores[holeIdx] = 0;
-      for (let round of this.awayTeamRounds) {
-        this.awayTeamHoleNetScores[holeIdx] += round.holes[holeIdx].net_score;
+      this.bottomTeamHoleNetScores[holeIdx] = 0;
+      for (let round of this.bottomTeamRounds) {
+        this.bottomTeamHoleNetScores[holeIdx] += round.holes[holeIdx].net_score;
       }
     }
   }
 
   private setWinningTeamId(): void {
-    let homeTeamTotalNetScore = this.homeTeamHoleNetScores.reduce(function(prev: number, cur: number) {
+    let topTeamTotalNetScore = this.topTeamHoleNetScores.reduce(function(prev: number, cur: number) {
       return prev + cur;
     }, 0);
-    let awayTeamTotalNetScore = this.awayTeamHoleNetScores.reduce(function(prev: number, cur: number) {
+    let bottomTeamTotalNetScore = this.bottomTeamHoleNetScores.reduce(function(prev: number, cur: number) {
       return prev + cur;
     }, 0);
-    this.winningTeamId = homeTeamTotalNetScore < awayTeamTotalNetScore ? this.homeTeamId : awayTeamTotalNetScore < homeTeamTotalNetScore ? this.awayTeamId : -1;
+    this.winningTeamId = topTeamTotalNetScore < bottomTeamTotalNetScore ? this.topTeamId : bottomTeamTotalNetScore < topTeamTotalNetScore ? this.bottomTeamId : -1;
   }
 }
--- apl-golf-league/src/app/matches/match-scorecard/match-scorecard.component.ts
+++ apl-golf-league/src/app/matches/match-scorecard/match-scorecard.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from "@angular/core";
 
 import { MatchData } from "src/app/shared/match.model";
 
@@ -7,14 +7,36 @@ import { MatchData } from "src/app/shared/match.model";
   templateUrl: "./match-scorecard.component.html",
   styleUrls: ["./match-scorecard.component.css"]
 })
-export class MatchScorecardComponent {
+export class MatchScorecardComponent implements OnInit, OnChanges {
   @Input() match: MatchData;
+  @Input() topTeamId: number;
+  bottomTeamId: number;
+
   scoreMode: string = "gross";
 
   roundIdx: number = 0;
 
+  ngOnInit(): void {
+    this.setTopAndBottomTeamIds();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    if (changes.topTeamId) {
+      this.topTeamId = changes.topTeamId.currentValue;
+    }
+    this.setTopAndBottomTeamIds();
+  }
+
   onScoreModeChanged(scoreMode: string): void {
     this.scoreMode = scoreMode;
+  }
+
+  private setTopAndBottomTeamIds(): void {
+    if (!this.topTeamId) {
+      this.topTeamId = this.match.home_team_id;
+    }
+    this.bottomTeamId = this.topTeamId === this.match.home_team_id ? this.match.away_team_id : this.match.home_team_id;
   }
 
 }
