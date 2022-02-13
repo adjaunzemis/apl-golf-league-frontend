673793528a9a532dcfaab875ee342ad61218bece
Andris Jaunzemis <adjaunzemis@gmail.com>
2022-02-13 15:10:11 -0500

    Added placeholder league handicapping page

---
+++ apl-golf-league/src/app/handicaps/handicaps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-handicaps',
+  templateUrl: './handicaps.component.html',
+  styleUrls: ['./handicaps.component.css']
+})
+export class HandicapsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
