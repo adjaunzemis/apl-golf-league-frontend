60a5e24c5f338a0dd3256c4aaa5fd89f5c3a598a
Andris Jaunzemis <adjaunzemis@gmail.com>
2022-02-14 10:14:36 -0500

    Updated rules committee info with current committee members

--- apl-golf-league/src/app/rules/rules.component.ts
+++ apl-golf-league/src/app/rules/rules.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RulesComponent implements OnInit {
 
-  constructor() { }
+  rulesCommittee: RulesCommitteeMemberInfo[];
 
   ngOnInit(): void {
+    this.rulesCommittee = [
+      { name: "Eric Crossley", role: "Committee Chair", email: "Eric.Crossley@jhuapl.edu" },
+      { name: "Gary Gafke", role: "Member", email: "ggafke@gmail.com" },
+      { name: "Mark Mathews", role: "Member", email: "Mark.Mathews@jhuapl.edu" }
+    ]
   }
 
+  getCommitteeEmailList(): string {
+    let emailList = "";
+    for (const member of this.rulesCommittee) {
+      emailList += member.email + ";"
+    }
+    return emailList.substring(0, emailList.length - 1);
+  }
+
+}
+
+interface RulesCommitteeMemberInfo {
+  name: string
+  role: string
+  email: string
 }
