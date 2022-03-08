9eda7f1db248b7f4d5d986a0c74c6eab30dcc49a
Andris Jaunzemis <adjaunzemis@gmail.com>
2022-03-07 21:41:56 -0500

    Added auto-login

--- apl-golf-league/src/app/auth/auth.service.ts
+++ apl-golf-league/src/app/auth/auth.service.ts
@@ -11,6 +11,8 @@ import { User, UserInfo } from "../shared/user.model";
   providedIn: 'root'
 })
 export class AuthService {
+  private static LOCAL_STORAGE_USER = 'aplGolfUserData';
+
   user = new BehaviorSubject<User|null>(null);
 
   constructor(private http: HttpClient, private router: Router) { }
@@ -25,12 +27,36 @@ export class AuthService {
         const expirationDate = new Date(new Date().getTime() + resData.access_token_expires_in * 1000);
         const user = new User(resData.id, resData.username, resData.email, resData.name, resData.disabled, resData.access_token, expirationDate);
         this.user.next(user);
+        localStorage.setItem(AuthService.LOCAL_STORAGE_USER, JSON.stringify(user));
       }));
+  }
+
+  autoLogin() {
+    const userDataStr = localStorage.getItem(AuthService.LOCAL_STORAGE_USER);
+    if (!userDataStr) {
+      return;
+    }
+
+    const userData: {id: string, username: string, email: string, name: string, disabled: string, _token: string,  _tokenExpirationDate: Date} = JSON.parse(userDataStr);
+    const loadedUser: User = new User(
+      +userData.id,
+      userData.username,
+      userData.email,
+      userData.name,
+      userData.disabled === 'true',
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
   }
 
   logout(): void {
     this.user.next(null);
     this.router.navigate([`/auth/login`]);
+    localStorage.removeItem(AuthService.LOCAL_STORAGE_USER);
   }
 
   getUserInfo() {
