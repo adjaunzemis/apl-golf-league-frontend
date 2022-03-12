0b660e4b516c2d894ebc870849fcf0ba1ce23e37
Andris Jaunzemis <adjaunzemis@gmail.com>
2022-03-12 11:47:07 -0500

    Added more validation to new golfer data input

--- apl-golf-league/src/app/golfers/golfer-create/golfer-create.component.ts
+++ apl-golf-league/src/app/golfers/golfer-create/golfer-create.component.ts
@@ -1,14 +1,15 @@
 import { Component, Inject } from "@angular/core";
 import { FormControl, Validators } from "@angular/forms";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
-import { GolferAffiliation } from "src/app/shared/golfer.model";
+
+import { Golfer, GolferAffiliation } from "../../shared/golfer.model";
 
 @Component({
   templateUrl: './golfer-create.component.html'
 })
 export class GolferCreateComponent {
 
-  nameControl: FormControl = new FormControl(this.data.name, [Validators.required, Validators.minLength(3), Validators.maxLength(25)]);
+  nameControl: FormControl = new FormControl(this.data.name, [Validators.required, Validators.minLength(3), Validators.maxLength(25), Validators.pattern('^[a-zA-Z ]*$')]);
   affiliationControl: FormControl = new FormControl(this.data.affiliation, [Validators.required]);
   emailControl: FormControl = new FormControl(this.data.email, [Validators.required, Validators.email]);
   phoneControl: FormControl = new FormControl(this.data.phone, []);
@@ -23,9 +24,13 @@ export class GolferCreateComponent {
   constructor(public dialogRef: MatDialogRef<GolferCreateComponent>, @Inject(MAT_DIALOG_DATA) public data: { name: string, affiliation: GolferAffiliation, email: string, phone: string }) { }
 
   onSubmit(): void {
-    const golferData = {
-      name: this.nameControl.value,
-      affilation: (this.affiliationControl.value as GolferAffiliation),
+    let golferName: string = this.nameControl.value;
+    golferName = golferName.split(' ').map(namePart => (namePart.charAt(0).toUpperCase() + namePart.slice(1))).join(' ').trim();
+
+    const golferData: Golfer = {
+      id: -1, // TODO: relax required 'id' value
+      name: golferName,
+      affiliation: (this.affiliationControl.value as GolferAffiliation),
       email: this.emailControl.value,
       phone: this.phoneControl.value
     };
