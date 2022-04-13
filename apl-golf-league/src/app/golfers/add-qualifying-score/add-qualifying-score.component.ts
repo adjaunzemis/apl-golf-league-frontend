418806c5989caa5694df97f26c1af01d71dce364
Andris Jaunzemis <adjaunzemis@gmail.com>
2022-04-12 23:01:59 -0400

    Updated qualifying score page with golfer selector

--- apl-golf-league/src/app/golfers/add-qualifying-score/add-qualifying-score.component.ts
+++ apl-golf-league/src/app/golfers/add-qualifying-score/add-qualifying-score.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { FormControl, Validators } from "@angular/forms";
+import { Observable, Subscription } from "rxjs";
+import { map, startWith } from "rxjs/operators";
+
+import { Golfer } from "../../shared/golfer.model";
+import { GolfersService } from "../golfers.service";
 
 @Component({
   selector: 'app-add-qualifying-score',
@@ -7,6 +12,7 @@ import { FormControl, Validators } from "@angular/forms";
   styleUrls: ['./add-qualifying-score.component.css']
 })
 export class AddQualifyingScoreComponent implements OnInit, OnDestroy {
+  isLoading = true;
 
   typeOptions: string[] = [
     "Qualifying Round",
@@ -18,12 +24,71 @@ export class AddQualifyingScoreComponent implements OnInit, OnDestroy {
   handicapIndexControl = new FormControl('', Validators.required);
   commentControl = new FormControl('');
 
-  ngOnInit(): void {
+  golferControl = new FormControl('');
 
+  private golfersSub: Subscription;
+  golferOptions: Golfer[] = [];
+  filteredGolferOptions: Observable<Golfer[]>;
+  selectedGolfer: Golfer | null;
+
+  constructor(private golfersService: GolfersService) { }
+
+  ngOnInit(): void {
+    this.golfersSub = this.golfersService.getAllGolfersUpdateListener().subscribe(result => {
+      this.golferOptions = result.sort((a: Golfer, b: Golfer) => {
+        if (a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      });
+      this.isLoading = false;
+    });
+
+    this.filteredGolferOptions = this.golferControl.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+        let golferName: string;
+        if (this.isGolfer(value)) {
+          golferName = value.name;
+        } else {
+          golferName = value;
+        }
+        const golfer = this.getGolferByName(golferName);
+        if (golfer) {
+          this.selectedGolfer = golfer;
+        } else {
+          this.selectedGolfer = null;
+        }
+        return this._filter(golferName);
+      }),
+    );
+
+    this.golfersService.getAllGolfers();
   }
 
   ngOnDestroy(): void {
+    this.golfersSub.unsubscribe();
+  }
 
+  private isGolfer(object: any): object is Golfer {
+    return (<Golfer> object).name !== undefined;
+  }
+
+  private getGolferByName(name: string): Golfer | null {
+    for (const golfer of this.golferOptions) {
+      if (golfer.name.toLowerCase() === name.toLowerCase()) {
+        return golfer;
+      }
+    }
+    return null;
+  }
+
+  private _filter(value: string): Golfer[] {
+    const filterValue = value.toLowerCase();
+    return this.golferOptions.filter(option => option.name.toLowerCase().includes(filterValue));
   }
 
 }
