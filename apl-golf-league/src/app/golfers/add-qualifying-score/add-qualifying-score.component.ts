929d0466ef19ac2826f493cece23a3b7b95656d0
Andris Jaunzemis <adjaunzemis@gmail.com>
2022-04-14 00:05:35 -0400

    Added form for qualifying score round 1 entry

--- apl-golf-league/src/app/golfers/add-qualifying-score/add-qualifying-score.component.ts
+++ apl-golf-league/src/app/golfers/add-qualifying-score/add-qualifying-score.component.ts
@@ -1,8 +1,8 @@
 import { QualifyingScore } from './../../shared/qualifying-score.model';
 import { Component, OnInit, OnDestroy } from "@angular/core";
-import { FormControl, Validators } from "@angular/forms";
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { Observable, Subscription } from "rxjs";
-import { map, startWith } from "rxjs/operators";
+import { map, min, startWith } from "rxjs/operators";
 
 import { Golfer } from "../../shared/golfer.model";
 import { GolfersService } from "../golfers.service";
@@ -15,6 +15,8 @@ import { GolfersService } from "../golfers.service";
 export class AddQualifyingScoreComponent implements OnInit, OnDestroy {
   isLoading = true;
 
+  golferControl = new FormControl('');
+
   typeOptions: string[] = [
     "Qualifying Round",
     "Official Handicap Index"
@@ -26,14 +28,45 @@ export class AddQualifyingScoreComponent implements OnInit, OnDestroy {
   handicapIndexControl = new FormControl('', Validators.required);
   commentControl = new FormControl('');
 
-  golferControl = new FormControl('');
+  round1Group = this.formBuilder.group({
+    datePlayed: [''],
+    courseName: [''],
+    trackName: [''],
+    teeName: [''],
+    teeGender: [''],
+    teeRating: [''],
+    teeSlope: [''],
+    comment: [''],
+    pars: this.formBuilder.group({
+      hole1: [''],
+      hole2: [''],
+      hole3: [''],
+      hole4: [''],
+      hole5: [''],
+      hole6: [''],
+      hole7: [''],
+      hole8: [''],
+      hole9: [''],
+    }),
+    scores: this.formBuilder.group({
+      hole1: [''],
+      hole2: [''],
+      hole3: [''],
+      hole4: [''],
+      hole5: [''],
+      hole6: [''],
+      hole7: [''],
+      hole8: [''],
+      hole9: [''],
+    })
+  });
 
   private golfersSub: Subscription;
   golferOptions: Golfer[] = [];
   filteredGolferOptions: Observable<Golfer[]>;
   selectedGolfer: Golfer | null;
 
-  constructor(private golfersService: GolfersService) { }
+  constructor(private golfersService: GolfersService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.golfersSub = this.golfersService.getAllGolfersUpdateListener().subscribe(result => {
@@ -82,6 +115,7 @@ export class AddQualifyingScoreComponent implements OnInit, OnDestroy {
     this.handicapIndexDateControl.setValue("");
     this.handicapIndexControl.setValue("");
     this.commentControl.setValue("");
+    this.round1Group.reset();
   }
 
   submitForm(): void {
@@ -95,6 +129,45 @@ export class AddQualifyingScoreComponent implements OnInit, OnDestroy {
       console.error("Invalid qualifying score type: " + this.typeControl.value);
       return;
     }
+  }
+
+  computeRound1TotalPar(): number {
+    return this.round1Group.get('pars')?.get('hole1')?.value as number
+      + this.round1Group.get('pars')?.get('hole2')?.value as number
+      + this.round1Group.get('pars')?.get('hole3')?.value as number
+      + this.round1Group.get('pars')?.get('hole4')?.value as number
+      + this.round1Group.get('pars')?.get('hole5')?.value as number
+      + this.round1Group.get('pars')?.get('hole6')?.value as number
+      + this.round1Group.get('pars')?.get('hole7')?.value as number
+      + this.round1Group.get('pars')?.get('hole8')?.value as number
+      + this.round1Group.get('pars')?.get('hole9')?.value as number;
+  }
+
+  computeRound1TotalGrossScore(): number {
+    return this.round1Group.get('scores')?.get('hole1')?.value as number
+      + this.round1Group.get('scores')?.get('hole2')?.value as number
+      + this.round1Group.get('scores')?.get('hole3')?.value as number
+      + this.round1Group.get('scores')?.get('hole4')?.value as number
+      + this.round1Group.get('scores')?.get('hole5')?.value as number
+      + this.round1Group.get('scores')?.get('hole6')?.value as number
+      + this.round1Group.get('scores')?.get('hole7')?.value as number
+      + this.round1Group.get('scores')?.get('hole8')?.value as number
+      + this.round1Group.get('scores')?.get('hole9')?.value as number;
+  }
+
+  computeRound1TotalAdjustedGrossScore(): number {
+    let adjustedGrossScore = 0;
+    for (const holeNum of [1, 2, 3, 4, 5, 6, 7, 8, 9]) {
+      adjustedGrossScore += Math.min(this.round1Group.get('scores')?.get('hole' + holeNum)?.value, this.round1Group.get('pars')?.get('hole' + holeNum)?.value + 5);
+    }
+    return adjustedGrossScore;
+  }
+
+  computeRound1ScoreDifferential(): number {
+    const rating = (this.round1Group.get('teeRating')?.value as number);
+    const slope = (this.round1Group.get('teeSlope')?.value as number);
+    const score = this.computeRound1TotalAdjustedGrossScore();
+    return (113.0 / slope) * (score - rating);
   }
 
   private submitOfficialHandicapIndex(): void {
@@ -125,6 +198,10 @@ export class AddQualifyingScoreComponent implements OnInit, OnDestroy {
   }
 
   private submitQualifyingScores(): void {
+    if (!this.round1Group.valid) {
+      console.error("Invalid input for 'Qualifying Round' type, cannot submit form!")
+      return;
+    }
     console.error("Not implemented yet!");
   }
 
