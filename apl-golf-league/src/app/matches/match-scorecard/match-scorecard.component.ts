1cda51910385049037b6b960f907097187df868c
Andris Jaunzemis <adjaunzemis@gmail.com>
2022-04-17 22:18:17 -0400

    Changed 'PH' to 'Hcp' on scorecard views for clarity

--- apl-golf-league/src/app/matches/match-scorecard/match-scorecard.component.ts
+++ apl-golf-league/src/app/matches/match-scorecard/match-scorecard.component.ts
@@ -44,7 +44,7 @@ export class MatchScorecardComponent implements OnInit, OnChanges {
   }
 
   getRoundSubtitle(round: RoundData): string {
-    return round.tee_name + " - PH: " + (round.golfer_playing_handicap ? round.golfer_playing_handicap.toFixed(0) : '--');
+    return round.tee_name + " - Hcp: " + (round.golfer_playing_handicap ? round.golfer_playing_handicap.toFixed(0) : '--');
   }
 
 }
