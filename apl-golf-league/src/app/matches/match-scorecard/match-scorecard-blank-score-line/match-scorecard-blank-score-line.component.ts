0b0a467805bc3d85f9abbaeeb1d3f57b2188a51a
Andris Jaunzemis <adjaunzemis@gmail.com>
2022-04-21 22:57:59 -0400

    Added match score line data based on round inputs

--- apl-golf-league/src/app/matches/match-scorecard/match-scorecard-blank-score-line/match-scorecard-blank-score-line.component.ts
+++ apl-golf-league/src/app/matches/match-scorecard/match-scorecard-blank-score-line/match-scorecard-blank-score-line.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input, OnChanges, OnInit } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
-import { MatchData } from "src/app/shared/match.model";
 import { RoundData } from "src/app/shared/round.model";
 
 @Component({
@@ -8,64 +7,15 @@ import { RoundData } from "src/app/shared/round.model";
   templateUrl: "./match-scorecard-blank-score-line.component.html",
   styleUrls: ["./match-scorecard-blank-score-line.component.css"]
 })
-export class MatchScorecardBlankScoreLineComponent implements OnInit, OnChanges {
-  // @Input() match: MatchData;
+export class MatchScorecardBlankScoreLineComponent {
+  @Input() topTeamRound: RoundData;
+  @Input() bottomTeamRound: RoundData;
 
-  // @Input() topTeamId: number;
-  // @Input() bottomTeamId: number;
+  @Input() editMode: boolean = false;
 
-  // topTeamRounds: RoundData[];
-  // bottomTeamRounds: RoundData[];
-
-  // topTeamHoleNetScores: number[];
-  // bottomTeamHoleNetScores: number[];
-
-  // winningTeamId: number;
-
-  ngOnInit(): void {
-    // this.separateRoundsByTeam();
-    // this.computeHoleNetScores();
-    // this.setWinningTeamId();
+  getNetScoreDifference(): number {
+    const topTeamNetScore = this.topTeamRound.holes.map(hole => hole.gross_score - hole.handicap_strokes).reduce((prev, next) => prev + next);
+    const bottomTeamNetScore = this.bottomTeamRound.holes.map(hole => hole.gross_score - hole.handicap_strokes).reduce((prev, next) => prev + next);
+    return topTeamNetScore - bottomTeamNetScore;
   }
-
-  ngOnChanges(): void {
-    // this.ngOnInit();
-  }
-
-  // private separateRoundsByTeam() {
-  //   this.topTeamRounds = [];
-  //   this.bottomTeamRounds = [];
-  //   for (let round of this.match.rounds) {
-  //     if (round.team_id === this.topTeamId) {
-  //       this.topTeamRounds.push(round);
-  //     } else if (round.team_id === this.bottomTeamId) {
-  //       this.bottomTeamRounds.push(round);
-  //     }
-  //   }
-  // }
-
-  // private computeHoleNetScores() {
-  //   this.topTeamHoleNetScores = [];
-  //   this.bottomTeamHoleNetScores = [];
-  //   for (let holeIdx = 0; holeIdx < this.match.rounds[0].holes.length; holeIdx++) {
-  //     this.topTeamHoleNetScores[holeIdx] = 0;
-  //     for (let round of this.topTeamRounds) {
-  //       this.topTeamHoleNetScores[holeIdx] += round.holes[holeIdx].net_score;
-  //     }
-  //     this.bottomTeamHoleNetScores[holeIdx] = 0;
-  //     for (let round of this.bottomTeamRounds) {
-  //       this.bottomTeamHoleNetScores[holeIdx] += round.holes[holeIdx].net_score;
-  //     }
-  //   }
-  // }
-
-  // private setWinningTeamId(): void {
-  //   let topTeamTotalNetScore = this.topTeamHoleNetScores.reduce(function(prev: number, cur: number) {
-  //     return prev + cur;
-  //   }, 0);
-  //   let bottomTeamTotalNetScore = this.bottomTeamHoleNetScores.reduce(function(prev: number, cur: number) {
-  //     return prev + cur;
-  //   }, 0);
-  //   this.winningTeamId = topTeamTotalNetScore < bottomTeamTotalNetScore ? this.topTeamId : bottomTeamTotalNetScore < topTeamTotalNetScore ? this.bottomTeamId : -1;
-  // }
 }
