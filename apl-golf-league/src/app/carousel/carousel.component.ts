efcbcdc069e62da44282f42abd3937555feab0f6
Andris Jaunzemis <adjaunzemis@gmail.com>
2022-12-06 09:42:49 -0500

    Finished carousel tutorial, started customization

--- apl-golf-league/src/app/carousel/carousel.component.ts
+++ apl-golf-league/src/app/carousel/carousel.component.ts
@@ -7,8 +7,33 @@ import { CarouselContents } from "../shared/carousel-contents.model";
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent {
-  @Input() contents: CarouselContents[] = [];
+  @Input() contents: CarouselContents[] = [
+    {"background_image_url": "/assets/apl_golf_logo.png"},
+    {"background_image_url": "/assets/trophy.png"}
+  ];
 
   currentIndex: number = 0;
+
+  goToNext(): void {
+    this.currentIndex += 1;
+    if (this.currentIndex >= this.contents.length) {
+      this.currentIndex = 0;
+    }
+  }
+
+  goToPrevious(): void {
+    this.currentIndex -= 1;
+    if (this.currentIndex < 0) {
+      this.currentIndex = this.contents.length - 1;
+    }
+  }
+
+  goToIndex(idx: number): void {
+    this.currentIndex = idx;
+  }
+
+  getCurrentBackgroundImageUrl(): string {
+    return `${this.contents[this.currentIndex].background_image_url}`
+  }
 
 }
