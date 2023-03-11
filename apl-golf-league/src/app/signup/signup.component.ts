9ae52d3d34dc547052774b315847674290f9a83d
Andris Jaunzemis <adjaunzemis@gmail.com>
2023-03-11 15:37:52 -0500

    Added tournament sign-ups to generic sign-up component

--- apl-golf-league/src/app/signup/signup.component.ts
+++ apl-golf-league/src/app/signup/signup.component.ts
@@ -6,6 +6,8 @@ import { map, startWith } from 'rxjs/operators';
 
 import { FlightData, FlightInfo } from '../shared/flight.model';
 import { FlightsService } from '../flights/flights.service';
+import { TournamentData, TournamentInfo } from '../shared/tournament.model';
+import { TournamentsService } from '../tournaments/tournaments.service';
 import { Golfer, GolferAffiliation } from '../shared/golfer.model';
 import { GolfersService } from '../golfers/golfers.service';
 import { GolferCreateComponent } from '../golfers/golfer-create/golfer-create.component';
@@ -22,19 +24,31 @@ export class SignupComponent implements OnInit, OnDestroy {
   private currentYear: number;
   currentDate = new Date();
 
-  isLoadingFlights = true;
   isLoadingGolfers = true;
+
+  isLoadingFlights = true;
   isLoadingSelectedFlight = false;
   isSelectedFlightSignupWindowOpen = false;
+
+  isLoadingTournaments = true;
+  isLoadingSelectedTournament = false;
+  isSelectedTournamentSignupWindowOpen = false;
 
   teamNameControl = new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]);
 
   flightControl = new FormControl('', Validators.required);
 
+  tournamentControl = new FormControl('', Validators.required);
+
   private flightsSub: Subscription;
   flights: FlightInfo[] = [];
   selectedFlight: FlightData;
   private selectedFlightSub: Subscription;
+
+  private tournamentsSub: Subscription;
+  tournaments: TournamentInfo[] = [];
+  selectedTournament: TournamentData;
+  private selectedTournamentSub: Subscription;
 
   private golfersSub: Subscription;
   golferOptions: Golfer[] = [];
@@ -44,7 +58,7 @@ export class SignupComponent implements OnInit, OnDestroy {
 
   newTeamForm: FormGroup;
 
-  constructor(private appConfigService: AppConfigService, private flightsService: FlightsService, private golfersService: GolfersService, private formBuilder: FormBuilder, private dialog: MatDialog) { }
+  constructor(private appConfigService: AppConfigService, private flightsService: FlightsService, private tournamentsService: TournamentsService, private golfersService: GolfersService, private formBuilder: FormBuilder, private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.currentYear = this.appConfigService.currentYear;
@@ -53,6 +67,14 @@ export class SignupComponent implements OnInit, OnDestroy {
       .subscribe(result => {
         this.flights = result.flights;
         this.isLoadingFlights = false;
+
+        this.golfersService.getAllGolfers();
+      });
+
+    this.tournamentsSub = this.tournamentsService.getTournamentsListUpdateListener()
+      .subscribe(result => {
+        this.tournaments = result.tournaments;
+        this.isLoadingTournaments = false;
 
         this.golfersService.getAllGolfers();
       });
@@ -82,6 +104,16 @@ export class SignupComponent implements OnInit, OnDestroy {
 
     this.flightsService.getFlightsList(this.currentYear);
 
+    this.selectedTournamentSub = this.tournamentsService.getTournamentUpdateListener()
+      .subscribe(result => {
+        this.selectedTournament = result;
+        this.isLoadingSelectedTournament = false;
+
+        this.isSelectedTournamentSignupWindowOpen = this.selectedTournament.signup_start_date <= this.currentDate && this.selectedTournament.signup_stop_date >= this.currentDate;
+      });
+
+    this.tournamentsService.getTournamentsList(this.currentYear);
+
     this.newTeamForm = this.formBuilder.group({
       teamGolfers: this.formBuilder.array([])
     });
@@ -91,6 +123,8 @@ export class SignupComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.flightsSub.unsubscribe();
     this.selectedFlightSub.unsubscribe();
+    this.tournamentsSub.unsubscribe();
+    this.selectedTournamentSub.unsubscribe();
     this.golfersSub.unsubscribe();
   }
 
@@ -99,11 +133,16 @@ export class SignupComponent implements OnInit, OnDestroy {
     this.flightsService.getFlight(id);
   }
 
+  getSelectedTournamentData(id: number): void {
+    this.isLoadingSelectedTournament = true;
+    this.tournamentsService.getTournament(id);
+  }
+
   getDaysUntilSignup(): number {
     return Math.floor((Date.UTC(this.selectedFlight.signup_start_date.getFullYear(), this.selectedFlight.signup_start_date.getMonth(), this.selectedFlight.signup_start_date.getDate()) - Date.UTC(this.currentDate.getFullYear(), this.currentDate.getMonth(), this.currentDate.getDate())) / (1000 * 60 * 60 * 24));
   }
 
-  onSubmitTeam(): void {
+  onSubmitFlightTeam(): void {
     // Extract new team signup info from form
     const newTeamName = this.teamNameControl.value as string;
 
@@ -151,6 +190,58 @@ export class SignupComponent implements OnInit, OnDestroy {
       error => {
         console.error(`Unable to create team ${newTeamName}`);
         this.isLoadingSelectedFlight = false
+      }
+    );
+  }
+
+  onSubmitTournamentTeam(): void {
+    // Extract new team signup info from form
+    const newTeamName = this.teamNameControl.value as string;
+
+    let newTeamGolfers: { golfer: Golfer, role: string, division: DivisionData }[] = [];
+    for (let idx = 0; idx < this.getTeamGolfersArray().length; idx++) {
+      const newTeamGolferForm = this.getTeamGolfersArray().at(idx);
+
+      const golferName = newTeamGolferForm.value.golfer as string;
+
+      const golferMatches = this.golferOptions.filter((g) => g.name === golferName);
+      if (golferMatches.length !== 1) {
+        this.dialog.open(ErrorDialogComponent, {
+          data: { title: "Team Sign-Up Error", message: `Invalid golfer name: ${golferName}` }
+        });
+        return;
+      }
+      const golfer = golferMatches[0];
+
+      newTeamGolfers.push({
+        golfer: golfer,
+        role: newTeamGolferForm.value.role as string,
+        division: newTeamGolferForm.value.division as DivisionData
+      });
+    }
+
+    // Submit new team signup
+    let golferData : { golfer_id: number, golfer_name: string, division_id: number, role: string }[] = [];
+
+    for (const newTeamGolfer of newTeamGolfers) {
+      golferData.push({
+        golfer_id: newTeamGolfer.golfer.id,
+        golfer_name: newTeamGolfer.golfer.name,
+        division_id: newTeamGolfer.division.id,
+        role: newTeamGolfer.role
+      });
+    }
+
+    this.isLoadingSelectedTournament = true
+    this.tournamentsService.createTeam(newTeamName, this.selectedTournament.id, golferData).subscribe(
+      team => {
+        console.log(`[TournamentSignupComponent] Created team '${team.name}' (id=${team.id})`);
+        this.clearSignupForms();
+        this.getSelectedTournamentData(this.selectedTournament.id); // refresh tournament info to get updated team
+      },
+      error => {
+        console.error(`Unable to create team ${newTeamName}`);
+        this.isLoadingSelectedTournament = false
       }
     );
   }
