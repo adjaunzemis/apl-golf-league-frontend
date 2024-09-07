a0f70d3175de0c61b702f0840e5ec8a3486c0ad5
Andris Jaunzemis <adjaunzemis@gmail.com>
2024-09-07 11:21:25 -0400

    Add version to environment config and footer

--- apl-golf-league/src/environments/environment.staging.ts
+++ apl-golf-league/src/environments/environment.staging.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://aplgolfapi.staging.jaunzenet.com/',
-  title: "APL Golf League - STAGING"
+  title: "APL Golf League - STAGING",
+  version: "STAGING"
 };
