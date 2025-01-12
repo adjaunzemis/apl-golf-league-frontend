543ec1b44cdd9f6cf54f0c5ad4ad1b76cb9e24b7
Andris Jaunzemis <adjaunzemis@gmail.com>
2025-01-12 07:30:16 -0500

    Added service to get API info

---
+++ apl-golf-league/src/app/api/api.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, Subject } from "rxjs";
+
+import { APIInfo } from './../shared/api.model';
+import { environment } from './../../environments/environment';
+
+@Injectable({
+  providedIn: "root"
+})
+export class APIService {
+  private info: APIInfo
+  private infoUpdated = new Subject<APIInfo>();
+
+  constructor(private http: HttpClient) {}
+
+  getInfo(): void {
+    this.http.get<APIInfo>(environment.apiUrl)
+      .subscribe(result => {
+        this.info = result;
+        this.infoUpdated.next(this.info);
+      });
+  }
+
+  getInfoUpdateListener(): Observable<APIInfo> {
+    return this.infoUpdated.asObservable();
+  }
+
+}
+
