a311843509c7a7dba5edc7aa110f3525a721da8f
Andris Jaunzemis <adjaunzemis@gmail.com>
2025-01-20 21:48:16 -0500

    Reorganize repository (#5)
    * Moved files to top-level directory
    
    * Updated deployment scripts
    
    * Add dependabot config

---
+++ src/app/shared/tournament.model.ts
@@ -0,0 +1,74 @@
+import { DivisionCreate, DivisionData } from "./division.model";
+import { TournamentTeamData } from "./team.model";
+
+export interface TournamentData {
+    id: number
+    year: number
+    name: string
+    date: Date
+    course_id: number
+    course: string
+    logo_url?: string
+    secretary?: string
+    secretary_email?: string
+    signup_start_date: Date
+    signup_stop_date: Date
+    members_entry_fee: number
+    non_members_entry_fee: number
+    divisions: DivisionData[]
+    teams: TournamentTeamData[]
+    shotgun: boolean
+    strokeplay: boolean
+    bestball: number
+    scramble: boolean
+    ryder_cup: boolean
+    individual: boolean
+    chachacha: boolean
+}
+
+export interface TournamentInfo {
+  id: number
+  year: number
+  name: string
+  date: Date
+  secretary?: string
+  secretary_email?: string
+  signup_start_date: Date
+  signup_stop_date: Date
+  members_entry_fee: number
+  non_members_entry_fee: number
+  course: string
+  logo_url?: string
+  shotgun: boolean
+  strokeplay: boolean
+  bestball: number
+  scramble: boolean
+  ryder_cup: boolean
+  individual: boolean
+  chachacha: boolean
+}
+
+export interface TournamentCreate {
+  id?: number
+  name: string
+  year: number
+  course_id: number
+  logo_url: string
+  secretary: string
+  secretary_email: string
+  secretary_phone?: string
+  signup_start_date: Date
+  signup_stop_date: Date
+  date: Date
+  members_entry_fee: number
+  non_members_entry_fee: number
+  bestball: number
+  shotgun: boolean
+  strokeplay: boolean
+  scramble: boolean
+  individual: boolean
+  ryder_cup: boolean
+  chachacha: boolean
+  locked?: boolean
+  divisions: DivisionCreate[]
+}
