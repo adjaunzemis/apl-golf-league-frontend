24c20e6a273ebac1a3b1f1b551f09b1283c517e9
Andris Jaunzemis <adjaunzemis@gmail.com>
2025-01-22 10:32:41 -0500

    Fix timeout type

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -12,7 +12,7 @@ import { User, UserInfo } from '../shared/user.model';
 })
 export class AuthService {
   private static LOCAL_STORAGE_USER = 'aplGolfUserData';
-  private tokenExpirationTimer: NodeJS.Timeout | null = null;
+  private tokenExpirationTimer: ReturnType<typeof setTimeout> | null = null;
 
   user = new BehaviorSubject<User | null>(null);
 
