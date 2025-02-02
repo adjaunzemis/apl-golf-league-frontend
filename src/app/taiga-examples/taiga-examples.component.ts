8aa280dcbc3e32c1ec89c9273b35bb3fee1fbed9
Andris Jaunzemis <adjaunzemis@gmail.com>
2025-02-01 23:17:15 -0500

    Add example line chart

--- src/app/taiga-examples/taiga-examples.component.ts
+++ src/app/taiga-examples/taiga-examples.component.ts
@@ -1,7 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TuiRepeatTimes } from '@taiga-ui/cdk';
+import { TuiAxes, TuiLineChart } from '@taiga-ui/addon-charts';
 import { TuiAppearance, TuiButton, TuiIcon, TuiTitle } from '@taiga-ui/core';
 import { TuiCardLarge, TuiCardMedium, TuiHeader } from '@taiga-ui/layout';
+import type { TuiContext, TuiStringHandler } from '@taiga-ui/cdk';
+import type { TuiPoint } from '@taiga-ui/core';
+import { TuiHint } from '@taiga-ui/core';
 
 @Component({
   standalone: true,
@@ -17,7 +21,23 @@ import { TuiCardLarge, TuiCardMedium, TuiHeader } from '@taiga-ui/layout';
     TuiIcon,
     TuiRepeatTimes,
     TuiTitle,
+    TuiAxes,
+    TuiLineChart,
+    TuiHint,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TaigaExamplesComponent {}
+export class TaigaExamplesComponent {
+  protected readonly value: readonly TuiPoint[] = [
+    [50, 50],
+    [100, 75],
+    [150, 50],
+    [200, 150],
+    [250, 155],
+    [300, 190],
+    [350, 90],
+  ];
+
+  protected readonly hint: TuiStringHandler<TuiContext<TuiPoint>> = ({ $implicit }) =>
+    `Vertical: ${$implicit[1]}\nHorizontal: ${$implicit[0]}`;
+}
