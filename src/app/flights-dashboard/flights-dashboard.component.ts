991a159ee8ba01546fb04ae5f73173e62d256730
Andris Jaunzemis <adjaunzemis@gmail.com>
2025-02-20 23:17:44 -0500

    Add routerlinks to flights list items

--- src/app/flights-dashboard/flights-dashboard.component.ts
+++ src/app/flights-dashboard/flights-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { CardModule } from 'primeng/card';
 import { DataViewModule } from 'primeng/dataview';
@@ -9,31 +10,30 @@ import { FlightInfo } from '../shared/flight.model';
 import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-flights-dashboard',
-    templateUrl: './flights-dashboard.component.html',
-    styleUrls: ['./flights-dashboard.component.css'],
-    standalone: true,
-    imports: [CommonModule, CardModule, DataViewModule, ProgressSpinnerModule],
-    providers: [FlightsService]
+  selector: 'app-flights-dashboard',
+  templateUrl: './flights-dashboard.component.html',
+  styleUrls: ['./flights-dashboard.component.css'],
+  standalone: true,
+  imports: [CommonModule, RouterModule, CardModule, DataViewModule, ProgressSpinnerModule],
+  providers: [FlightsService],
 })
 export class FlightsDashboardComponent implements OnInit, OnDestroy {
+  isLoading = true;
+  flights = signal<FlightInfo[]>([]);
+  private flightsSub: Subscription;
 
-    isLoading = true;
-    flights = signal<FlightInfo[]>([]);
-    private flightsSub: Subscription;
+  private flightsService = inject(FlightsService);
 
-    private flightsService = inject(FlightsService);
+  ngOnInit(): void {
+    this.flightsSub = this.flightsService.getFlightsListUpdateListener().subscribe((result) => {
+      console.log(`[FlightsDashboardComponent] Received list of ${result.numFlights} flights`);
+      this.flights.set([...result.flights]);
+      this.isLoading = false;
+    });
+    this.flightsService.getFlightsList();
+  }
 
-    ngOnInit(): void {
-        this.flightsSub = this.flightsService.getFlightsListUpdateListener().subscribe((result) => {
-            console.log(`[FlightsDashboardComponent] Received list of ${result.numFlights} flights`);
-            this.flights.set([...result.flights]);
-            this.isLoading = false;
-        });
-        this.flightsService.getFlightsList();
-    }
-
-    ngOnDestroy(): void {
-        this.flightsSub.unsubscribe();
-    }
+  ngOnDestroy(): void {
+    this.flightsSub.unsubscribe();
+  }
 }
