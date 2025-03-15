28077abdccb7139207a930a5bcb35b2743c2d372
Andris Jaunzemis <adjaunzemis@gmail.com>
2025-03-15 00:59:19 -0400

    Added weekly flight schedule carousel

--- src/app/flights/flight-homepage/flight-schedule/flight-schedule.component.ts
+++ src/app/flights/flight-homepage/flight-schedule/flight-schedule.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { CardModule } from 'primeng/card';
+import { TabsModule } from 'primeng/tabs';
 
 import { FlightInfo } from 'src/app/shared/flight.model';
 import { MatchSummary } from 'src/app/shared/match.model';
+import { FlightScheduleWeeklyComponent } from './flight-schedule-weekly/flight-schedule-weekly.component';
 import { FlightScheduleMatrixComponent } from './flight-schedule-matrix/flight-schedule-matrix.component';
 
 @Component({
   selector: 'app-flight-schedule',
   templateUrl: './flight-schedule.component.html',
   styleUrl: './flight-schedule.component.css',
-  imports: [CardModule, FlightScheduleMatrixComponent],
+  imports: [CardModule, FlightScheduleWeeklyComponent, FlightScheduleMatrixComponent, TabsModule],
 })
 export class FlightScheduleComponent {
   @Input() info: FlightInfo;
