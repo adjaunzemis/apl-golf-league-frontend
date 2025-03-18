9b235306641c0413902e420e751352e4f44c9256
Andris Jaunzemis <adjaunzemis@gmail.com>
2025-03-18 17:41:46 -0400

    Update team golfer table styling

--- src/app/flights/flight-homepage/flight-teams/flight-teams.component.ts
+++ src/app/flights/flight-homepage/flight-teams/flight-teams.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
 import { AccordionModule } from 'primeng/accordion';
+import { TableModule } from 'primeng/table';
 
 import { FlightTeam } from 'src/app/shared/flight.model';
 
@@ -9,7 +10,7 @@ import { FlightTeam } from 'src/app/shared/flight.model';
   selector: 'app-flight-teams',
   templateUrl: './flight-teams.component.html',
   styleUrl: './flight-teams.component.css',
-  imports: [CommonModule, CardModule, AccordionModule],
+  imports: [CommonModule, CardModule, AccordionModule, TableModule],
 })
 export class FlightTeamsComponent {
   @Input() teams: FlightTeam[];
