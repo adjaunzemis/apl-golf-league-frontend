ce8591d3bdd4b23086ced16f377dbbd799dc0d13
Andris Jaunzemis <adjaunzemis@gmail.com>
2025-03-22 08:09:21 -0400

    Styling adjustments for header

--- src/app/primeng.module.ts
+++ src/app/primeng.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
+import { SharedModule } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { ImageModule } from 'primeng/image';
 import { CardModule } from 'primeng/card';
 import { DialogModule } from 'primeng/dialog';
 import { MenubarModule } from 'primeng/menubar';
+import { TabViewModule } from 'primeng/tabview';
 import { BadgeModule } from 'primeng/badge';
 import { DrawerModule } from 'primeng/drawer';
 
 @NgModule({
   exports: [
+    SharedModule,
     ButtonModule,
     ImageModule,
     CardModule,
     DialogModule,
     MenubarModule,
+    TabViewModule,
     BadgeModule,
     DrawerModule,
   ],
