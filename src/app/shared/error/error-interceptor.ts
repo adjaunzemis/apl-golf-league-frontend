aa1ddc797aaeee2c88a8910cb21f225036943349
Andris Jaunzemis <adjaunzemis@gmail.com>
2025-03-21 22:28:03 -0400

    Use notification service in error interceptor

--- src/app/shared/error/error-interceptor.ts
+++ src/app/shared/error/error-interceptor.ts
@@ -1,30 +1,28 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpErrorResponse, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Injectable, inject } from '@angular/core';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 import { delayedRetry } from './error-handling/delayed-retry';
+import { NotificationService } from 'src/app/notifications/notification.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private snackBar: MatSnackBar) {}
+  private notificationService = inject(NotificationService);
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return next.handle(req).pipe(
       delayedRetry(1000, 3),
       catchError((error: HttpErrorResponse) => {
+        const errorSummary = 'Unknown Error';
         let errorMessage =
           'Uh oh... An unknown error occurred! :( Please try again later. If this error persists, contact the webmaster.';
         if (error.error.detail) {
           errorMessage = error.error.detail;
         }
-        this.snackBar.open(errorMessage, undefined, {
-          duration: 5000,
-          panelClass: ['error-snackbar'],
-        });
-        return throwError(error);
+        this.notificationService.showError(errorSummary, errorMessage, 5000);
+        return throwError(() => error);
       }),
     );
   }
