279ef3deec338ad5cfe862d71eca63753850b5de
Andris Jaunzemis <adjaunzemis@gmail.com>
2025-03-22 00:21:36 -0400

    Updated login form to use primeng

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,12 +4,11 @@ import { RouterModule } from '@angular/router';
 
 import { AngularMaterialModule } from '../angular-material.module';
 import { ErrorModule } from '../shared/error/error.module';
-import { LoginComponent } from './login/login.component';
 import { UserManageComponent } from './user-manage/user-manage.component';
 import { UserHomeComponent } from './user-home/user-home.component';
 
 @NgModule({
-  declarations: [LoginComponent, UserManageComponent, UserHomeComponent],
+  declarations: [UserManageComponent, UserHomeComponent],
   imports: [CommonModule, RouterModule, AngularMaterialModule, ErrorModule],
 })
 export class AuthModule {}
