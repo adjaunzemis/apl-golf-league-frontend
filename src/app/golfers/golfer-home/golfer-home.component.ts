9699f114e9c9f02701d6913b6af4d33b92229049
Andris Jaunzemis <adjaunzemis@gmail.com>
2025-04-30 22:12:15 -0400

    Move service queries to home component

--- src/app/golfers/golfer-home/golfer-home.component.ts
+++ src/app/golfers/golfer-home/golfer-home.component.ts
@@ -1,22 +1,31 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { CardModule } from 'primeng/card';
+import { SelectModule } from 'primeng/select';
+import { TagModule } from 'primeng/tag';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 import { GolferInfoComponent } from './golfer-info/golfer-info.component';
 import { GolfersService } from '../golfers.service';
-import { GolferData } from 'src/app/shared/golfer.model';
+import { GolferData, TeamGolferData } from 'src/app/shared/golfer.model';
 import { Season } from 'src/app/shared/season.model';
+import { RoundData } from 'src/app/shared/round.model';
+import { RoundsService } from 'src/app/rounds/rounds.service';
+import { SeasonsService } from 'src/app/seasons/seasons.service';
 
 @Component({
   selector: 'app-golfer-home',
   templateUrl: './golfer-home.component.html',
   styleUrl: './golfer-home.component.css',
-  imports: [CommonModule, ProgressSpinnerModule, GolferInfoComponent],
+  imports: [CommonModule, FormsModule, CardModule, SelectModule, TagModule, ProgressSpinnerModule, GolferInfoComponent],
 })
 export class GolferHomeComponent implements OnInit, OnDestroy {
   isLoading = true;
+
+  golferId: number;
 
   private golferSub: Subscription;
   golfer: GolferData;
@@ -25,29 +34,77 @@ export class GolferHomeComponent implements OnInit, OnDestroy {
   seasons: Season[];
   selectedSeason: Season;
 
+  private teamsSub: Subscription;
+  flightTeams: TeamGolferData[] = [];
+  tournamentTeams: TeamGolferData[] = [];
+
+  private roundsSub: Subscription;
+  rounds: RoundData[] = [];
+
   private golfersService = inject(GolfersService);
+  private roundsService = inject(RoundsService);
+  private seasonsService = inject(SeasonsService);
 
   private route = inject(ActivatedRoute);
 
   ngOnInit(): void {
+    this.seasonsSub = this.seasonsService.getSeasons().subscribe((result) => {
+      this.seasons = [...result];
+      this.selectedSeason = result.filter((season) => season.is_active)[0];
+      this.getSelectedSeasonData();
+    });
+
     this.golferSub = this.golfersService.getGolferUpdateListener().subscribe((result) => {
       this.golfer = result;
+    });
 
-      this.isLoading = false;
+    this.roundsSub = this.roundsService.getRoundUpdateListener().subscribe((result) => {
+      console.log(`[GolferHomeComponent] Received ${result.length} rounds`);
+      if (result.length > 0) {
+        this.rounds = result;
+      } else {
+        this.rounds = [];
+      }
+      // this.organizeRoundsByTee();
+    });
+
+    this.teamsSub = this.golfersService.getGolferTeamDataUpdateListener().subscribe((result) => {
+      console.log(`[GolferHomeComponent] Received ${result.length} teams`);
+      this.flightTeams = result.filter((team) => team.flight_name);
+      this.tournamentTeams = result.filter((team) => team.tournament_name);
     });
 
     this.route.queryParams.subscribe((params) => {
       if (params && params.id) {
         console.log('[GolferHomeComponent] Processing route with query parameter: id=' + params.id);
-        const golferId = params.id;
+        this.golferId = params.id;
 
-        this.golfersService.getGolfer(golferId);
+        this.golfersService.getGolfer(this.golferId);
+        this.getSelectedSeasonData();
       }
     });
   }
 
   ngOnDestroy(): void {
-    this.golferSub.unsubscribe();
     this.seasonsSub.unsubscribe();
+    this.golferSub.unsubscribe();
+    this.teamsSub.unsubscribe();
+    this.roundsSub.unsubscribe();
+  }
+
+  onSeasonSelected(): void {
+    this.getSelectedSeasonData();
+  }
+
+  private getSelectedSeasonData(): void {
+    if (this.golferId && this.selectedSeason) {
+      console.log(`[GolferHomeComponent] Fetching golfer round data for year=${this.selectedSeason.year}`);
+      this.isLoading = true;
+
+      this.golfersService.getGolferTeamData(this.golferId, this.selectedSeason.year);
+      this.roundsService.getRounds(this.golferId, this.selectedSeason.year);
+
+      this.isLoading = false;
+    }
   }
 }
--- src/app/golfers/golfer-home/golfer-info/golfer-info.component.ts
+++ src/app/golfers/golfer-home/golfer-info/golfer-info.component.ts
@@ -1,38 +1,18 @@
-import { Component, inject, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Subscription } from 'rxjs';
 import { CardModule } from 'primeng/card';
-import { TagModule } from 'primeng/tag';
 
 import { GolferData } from 'src/app/shared/golfer.model';
-import { SelectModule } from 'primeng/select';
-import { SeasonsService } from 'src/app/seasons/seasons.service';
 import { Season } from 'src/app/shared/season.model';
 
 @Component({
   selector: 'app-golfer-info',
   templateUrl: './golfer-info.component.html',
   styleUrl: './golfer-info.component.css',
-  imports: [CommonModule, FormsModule, CardModule, SelectModule, TagModule],
+  imports: [CommonModule, FormsModule, CardModule],
 })
-export class GolferInfoComponent implements OnInit, OnDestroy {
+export class GolferInfoComponent {
   @Input() golfer: GolferData;
-
-  private seasonsSub: Subscription;
-  seasons: Season[];
-  selectedSeason: Season;
-
-  private seasonsService = inject(SeasonsService);
-
-  ngOnInit(): void {
-    this.seasonsSub = this.seasonsService.getSeasons().subscribe((result) => {
-      this.seasons = [...result];
-      this.selectedSeason = result.filter((season) => season.is_active)[0];
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.seasonsSub.unsubscribe();
-  }
+  @Input() season: Season;
 }
