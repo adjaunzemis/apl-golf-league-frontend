6086455a1932176b14f2e19ae7bb9b85d269b701
Andris Jaunzemis <adjaunzemis@gmail.com>
2025-05-02 10:59:18 -0400

    Added scoring record table

--- src/app/golfers/golfer-home/golfer-handicap/golfer-handicap.component.ts
+++ src/app/golfers/golfer-home/golfer-handicap/golfer-handicap.component.ts
@@ -1,17 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
 
 import { GolferData } from 'src/app/shared/golfer.model';
+import { TableModule } from 'primeng/table';
+import { RoundSummary } from 'src/app/shared/round.model';
+import { InitialsPipe } from 'src/app/shared/initials.pipe';
 
 @Component({
   selector: 'app-golfer-handicap',
   templateUrl: './golfer-handicap.component.html',
   styleUrl: './golfer-handicap.component.css',
-  imports: [CommonModule, CardModule],
+  imports: [CommonModule, CardModule, TableModule, InitialsPipe],
 })
-export class GolferHandicapComponent {
+export class GolferHandicapComponent implements OnInit {
   @Input() golfer: GolferData;
+
+  rounds!: RoundSummary[];
+
+  ngOnInit(): void {
+    this.rounds = [];
+    if (this.golfer.handicap_index_data && this.golfer.handicap_index_data.active_rounds) {
+      this.rounds.push(...this.golfer.handicap_index_data.active_rounds);
+    }
+  }
 
   getActiveIndex(): number | null {
     if (
