e0d06c9b598cf8d4fa073073303af9fe28ffcae4
Andris Jaunzemis <adjaunzemis@gmail.com>
2026-06-30 09:34:58 -0400

    Stabilize version drifts

--- eslint.config.js
+++ eslint.config.js
@@ -1,47 +1,26 @@
 // @ts-check
-const eslint = require('@eslint/js');
-const tseslint = require('typescript-eslint');
-const angular = require('angular-eslint');
-const prettier = require('eslint-config-prettier');
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+const prettier = require("eslint-config-prettier");
 
 module.exports = tseslint.config(
   {
-    files: ['**/*.ts'],
+    files: ["**/*.ts"],
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
-      ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
-      prettier,
+      prettier
     ],
-    processor: angular.processInlineTemplates,
-    rules: {
-      '@angular-eslint/directive-selector': [
-        'error',
-        {
-          type: 'attribute',
-          prefix: 'app',
-          style: 'camelCase',
-        },
-      ],
-      '@angular-eslint/component-selector': [
-        'error',
-        {
-          type: 'element',
-          prefix: 'app',
-          style: 'kebab-case',
-        },
-      ],
-      '@angular-eslint/prefer-standalone': 'off', // TODO: refactor to use standalone components
-    },
+    processor: angular.processInlineTemplates
   },
   {
-    files: ['**/*.html'],
+    files: ["**/*.html"],
     extends: [
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
-      prettier,
-    ],
-    rules: {},
-  },
+      prettier
+    ]
+  }
 );
