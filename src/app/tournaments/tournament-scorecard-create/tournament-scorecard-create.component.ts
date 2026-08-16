31796f6cbe1f3cd43b610ccb3b4a1584ae3e4a8e
Andris Jaunzemis <adjaunzemis@gmail.com>
2026-08-16 15:58:03 -0400

    Fix lint errors

--- src/app/tournaments/tournament-scorecard-create/tournament-scorecard-create.component.ts
+++ src/app/tournaments/tournament-scorecard-create/tournament-scorecard-create.component.ts
@@ -546,8 +546,8 @@ export class TournamentScorecardCreateComponent implements OnInit, OnDestroy {
     for (let holeIdx = 0; holeIdx < rounds[0].holes.length; holeIdx++) {
       const hole = rounds[0].holes[holeIdx];
       let holePar = hole.par;
-      let grossScore = 99;
-      let netScore = 99;
+      let grossScore;
+      let netScore;
       if (this.selectedTournamentData?.bestball === 2) {
         holePar = hole.par * 2;
         const grossScores = rounds
